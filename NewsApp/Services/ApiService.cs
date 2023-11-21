1a7d512f7241386a5bb59dcc23e38d9808fc3cf6
蔡侑典 <145438019+s1091902@users.noreply.github.com>
2023-11-21 15:26:13 +0800

    Update ApiService.cs

--- NewsApp/Services/ApiService.cs
+++ NewsApp/Services/ApiService.cs
@@ -8,7 +8,7 @@ public class ApiService
 	public async Task<Root> GetNews(string categoryName)
 	{
 		var httpClient = new HttpClient();
-		var response = await httpClient.GetStringAsync($"https://gnews.io/api/v4/top-headlines?category={categoryName.ToLower()}&apikey=9b53f5b0ff481b6ada9cfd143c03bedb&lang=zh");
+		var response = await httpClient.GetStringAsync($"https://gnews.io/api/v4/top-headlines?category={categoryName.ToLower()}&apikey=a0f69a37feb6ab395c35f77adb0fb207&lang=zh");
 		return JsonConvert.DeserializeObject<Root>(response);
 	}
 }
